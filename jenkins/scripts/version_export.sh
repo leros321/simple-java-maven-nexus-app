896713eda12092912c79a95cb9e28ccc55c3c3ff
root <root@openshift.novalocal>
2018-08-28 13:34:09 +0200

    updated Jenkins file

--- jenkins/scripts/version_export.sh
+++ jenkins/scripts/version_export.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'extracts the value of the <version/> element within <project/> instead.'
-set -x
-VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-set +x
+#echo 'extracts the value of the <version/> element within <project/> instead.'
+#set -x
+mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"
+#set +x
