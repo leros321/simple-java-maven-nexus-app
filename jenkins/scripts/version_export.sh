a79dc327b5b900e09e66210efc8d80aea83c4fe8
root <root@openshift.novalocal>
2018-08-28 13:29:23 +0200

    updated Jenkins file

---
+++ jenkins/scripts/version_export.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo 'extracts the value of the <version/> element within <project/> instead.'
+set -x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
